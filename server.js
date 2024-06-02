c481a92e2b2adbfa2011ddeba830a3ebeafbb412
Parikshit Jadhav <jadhavparikshit0510@gmail.com>
2024-06-02 11:18:27 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express= require('express')
+
+
+const app = express()
+const PORT = process.env.PORT || 6770;
+
+app.listen(PORT, ()=>{
+    console.log(`The server is runnig on ${PORT} `)
+})
