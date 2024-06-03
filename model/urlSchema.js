6ef4a40245983ecdbc0945e849ea6f23783cf119
Parikshit Jadhav <jadhavparikshit0510@gmail.com>
2024-06-03 12:03:00 +0530

    middleware and model defined

---
+++ model/urlSchema.js
@@ -0,0 +1,18 @@
+const { string, required } = require('joi');
+const mongoose = require('mongoose');
+
+
+const urlschema =new  mongoose.Schema({
+    shortId :{
+        type: String,
+        required :true,
+        unique : true
+    },
+    requiredurl:{
+        type: String,
+        required :true,
+    }
+})
+
+exports.urlmodel = mongoose.model('url-shortner',urlschema);
+
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express= require('express')
 const app = express()
 const PORT = process.env.PORT || 6770;
 
+
 app.listen(PORT, ()=>{
     console.log(`The server is runnig on ${PORT} `)
 })
+
